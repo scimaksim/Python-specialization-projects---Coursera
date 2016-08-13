74203dbb55d9d8c810b315d0fbf44457f5798023
scimaksim <maksim.ast@gmail.com>
2016-08-13 11:40:52 -0700

    json3.py

---
+++ Coursera/json3.py
@@ -0,0 +1,25 @@
+import urllib
+import json
+
+sum = 0
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+
+    url = address
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved',len(data),'characters'
+
+    info = json.loads(data)
+
+    print 'Number of items in dictionary:', len(info)
+    print 'User count:', len(info['comments'])
+
+    #Comments is a list of dictionary key-value pairs
+    for item in info['comments']:
+        sum += int(item['count'])
+
+    print 'The sum of all counts is:', sum
